4de58bc5f3b3e0594c86eabcaac3b0b05528b5fc
okugario <alcimic972@gmail.com>
2022-09-28 10:32:05 +0700

    Update to 0.1

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import '../Styles/HomePage.css';
+import * as React from 'react';
+import { Button } from 'antd-mobile';
 export default function HomePage(prop) {
   return (
     <>
-      <div className="angry-grid">
-        <div id="item-0">&nbsp;</div>
-        <div id="item-1">&nbsp;</div>
-        <div id="item-2">&nbsp;</div>
+      <header
+        style={{
+          background: '#001529',
+          color: '#fff',
+          textAlign: 'end',
+          height: '50px',
+        }}
+      >
+        <Button
+          color="default"
+          size="small"
+          style={{ marginTop: '10px', marginRight: '10px' }}
+        >
+          Выход
+        </Button>
+      </header>
+      <div
+        style={{
+          background: '#f5f5f5',
+          height: '600px',
+          color: 'black',
+          fontSize: '30px',
+          fontWeight: 'bold',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        Нет активных вкладок
       </div>
     </>
   );
